b495a42f5175be8a5b48317837ad4ec2ab04f45f
pamul1 <109163253+pamul1@users.noreply.github.com>
2024-09-11 21:01:20 -0500

    Commit6

--- script.js
+++ script.js
@@ -15,8 +15,18 @@ return area
 let area= calculateandreturnArea(6,9)
 console.log(area)
 
-function calculatetheTotalCost(){
     let items=[
-    {item:"Shirt", price: 15, quantity:1},
-    {item:"Shoe", price:120, quantity:1}]
+    {item:"Shirt", price: 15, quantity:3},
+    {item:"Shoe", price:120, quantity:1}
+]
+
+function calculatetheTotalCost(){
+    let totalCost=0
+    for (let i=0; i<items.length; i++){
+        totalitems+= totalCost[i].price * totalCost[i].quantity
+    }
+        return totalCost
+    
 }
+let result=calculatetheTotalCost()
+console.log(result)
