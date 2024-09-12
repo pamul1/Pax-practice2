e45cb99f8bb04e0127de2dcb360aa8656b3f28cc
pamul1 <109163253+pamul1@users.noreply.github.com>
2024-09-11 20:23:44 -0500

    Commit4

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function goodAfternoon(){
     console.log("How are you doing today")
 }
 goodAfternoon()
+
+function sum(a,b){
+    console.log(a+b)
+}
+sum(5,10)
+
+function calculateandreturnArea(width, height){
+let area=(width*height)
+return area
+}
+let area= calculateandreturnArea(6,9)
+console.log(area)
